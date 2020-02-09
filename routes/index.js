4b49f43685effed474ec10d1b19bfcb0c14491b6
AyoAyomide <ayoayomide124@gmail.com>
2020-02-09 10:06:44 +0100

    ui update

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/const', function(req, res, next) {
   res.render('const', { title: 'Express' });
 });
 
+router.get('/post', function(req, res, next) {
+  res.render('post', { title: 'Express' });
+});
+
+router.get('/hot', function(req, res, next) {
+  res.render('hot', { title: 'Express' });
+});
+
 module.exports = router;
